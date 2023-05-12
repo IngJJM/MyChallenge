156f3d5a22514e37b6720b8e98832369f876bca9
Jimmy <jimmy-45@hotmail.com>
2023-05-12 11:36:19 -0600

    Diseoño de aplicación con css y que sea responsiva junto con la funcion de encriptar mensajes de texto

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
 function encriptar(){
-    var texto=document.getElementById("inputTexto").value.toLowerCase();
+    var texto=document.getElementById("texto").value.toLowerCase();
+    var txt=document.getElementById("txt");
+    var txt2=document.getElementById("txt2");
+    var muñeco=document.getElementById("muñeco");
+
     var txtCifrado=texto.replace(/e/igm,"enter");
     var txtCifrado=txtCifrado.replace(/o/igm,"ober");
     var txtCifrado=txtCifrado.replace(/i/igm,"imes");
     var txtCifrado=txtCifrado.replace(/a/igm,"ai");
     var txtCifrado=txtCifrado.replace(/u/igm,"ufat");
 
-    document.getElementById("imgContainer3").style.display="none";
-    document.getElementById("txt").style.display="none";
-    document.getElementById("txt2").innerHTML=txtCifrado;
-    document.getElementById("copiar").style.display="show";
-    document.getElementById("copiar").style.display="inherit";
+    if (document.getElementById("texto").value.length !=0){
+        document.getElementById("txt").style.display="show";
+        document.getElementById("mensaje").innerHTML=txtCifrado;
+        document.getElementById("copiar").style.display="show";
+        document.getElementById("copiar").style.display="inherit";
+        txt.textContent="Texto encriptado exitosamente";
+        txt2.textContent="";
+        muñeco.src="./img/encriptado.jpg";
+    }else{
+        muñeco.src="img/Muñeco.png";
+        txt.textContent="Ningún mensaje fue encontrado";
+        txt2.textContent="Ingrese el texto que desees encriptar o desencriptar.";
+        alert("Debes ingresar algún texto");
+    }
 
 }
 function desencriptar(){
-    var texto=document.getElementById("inputTexto").value.toLowerCase();
+    var texto=document.getElementById("mensaje").value.toLowerCase();
     var txtCifrado=texto.replace(/enter/igm,"e");
     var txtCifrado=txtCifrado.replace(/ober/igm,"o");
     var txtCifrado=txtCifrado.replace(/imes/igm,"i");
     var txtCifrado=txtCifrado.replace(/ai/igm,"a");
     var txtCifrado=txtCifrado.replace(/ufat/igm,"u");
 
-    document.getElementById("imgContainer3").style.display="none";
+    document.getElementById("muñeco").style.display="none";
     document.getElementById("txt").style.display="none";
-    document.getElementById("txt2").innerHTML=txtCifrado;
+    document.getElementById("mensaje").innerHTML=txtCifrado;
     document.getElementById("copiar").style.display="show";
     document.getElementById("copiar").style.display="inherit";
 
