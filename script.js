940fff53cb97c9620cd260c1314e5a9b9015cfbf
Jimmy <jimmy-45@hotmail.com>
2023-05-18 13:05:10 -0600

    Mejoras de estilos, modificaciones ya que se encontraron algunos errores en cuanto al Script, mejoras en la interación con la pagina web.

--- script.js
+++ script.js
@@ -1,66 +1,68 @@
-function encriptar(){
-    var texto=document.getElementById("texto").value.toLowerCase();
-    var txt=document.getElementById("txt");
-    var txt2=document.getElementById("txt2");
-    var muñeco=document.getElementById("muñeco");
-    
-
-    var txtCifrado=texto.replace(/e/igm,"enter");
-    var txtCifrado=txtCifrado.replace(/o/igm,"ober");
-    var txtCifrado=txtCifrado.replace(/i/igm,"imes");
-    var txtCifrado=txtCifrado.replace(/a/igm,"ai");
-    var txtCifrado=txtCifrado.replace(/u/igm,"ufat");
-
-    if (document.getElementById("texto").value.length !=0){
-        document.getElementById("mensaje").innerHTML=txtCifrado;
-        txt.textContent="Texto encriptado exitosamente";
-        txt2.textContent="";
-        muñeco.src="img/encriptado.jpg";
-        
-    }else{
-        muñeco.src="img/Muñeco.png";
-        txt.textContent="Ningún mensaje fue encontrado";
-        txt2.textContent="Ingrese el texto que desees encriptar o desencriptar.";
-        swal({
-            title:"Debes ingresar algún texto", 
-            icon:"warning",
-            button:"OK"
-        });
-        document.getElementById("mensaje").value="";
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let txt = document.getElementById("txt");
+    let txt1 = document.getElementById("txt1");
+    let txt2 = document.getElementById("txt2");
+    let txt3 = document.getElementById("txt3");
+    let muñeco = document.getElementById("muñeco");
+   
+    //remplazar letras por palabras
+    let txtCifrado = texto
+      .replace(/e/gim, "enter")
+      .replace(/o/gim, "ober")
+      .replace(/i/gim, "imes")
+      .replace(/a/gim, "ai")
+      .replace(/u/gim, "ufat");
+  
+    if (texto.length != 0) {
+      document.getElementById("mensaje").value = txtCifrado;
+      document.getElementById("txt").style.display="none";
+      document.getElementById("txt1").style.display="block";
+      document.getElementById("txt2").style.display="none";
+      document.getElementById("txt3").style.display="none";
+      muñeco.src = "img/encriptado.jpg";
+      return;
+    } else {
+      muñeco.src = "img/Muñeco.png";
+      document.getElementById("txt").style.display="block";
+      document.getElementById("txt1").style.display="none";
+      document.getElementById("txt2").style.display="block";
+      document.getElementById("txt3").style.display="none";
+      document.getElementById("mensaje").value = "";
     }
-
-}
-
-function desencriptar(){
-    var texto=document.getElementById("mensaje").value.toLowerCase();
-    var txtCifrado=texto.replace(/enter/igm,"e");
-    var txtCifrado=txtCifrado.replace(/ober/igm,"o");
-    var txtCifrado=txtCifrado.replace(/imes/igm,"i");
-    var txtCifrado=txtCifrado.replace(/ai/igm,"a");
-    var txtCifrado=txtCifrado.replace(/ufat/igm,"u");
-
-    if (document.getElementById("texto").value.length !=0){
-        document.getElementById("mensaje").innerHTML=txtCifrado;
-        txt.textContent="Texto Desencriptado exitosamente";
-        txt2.textContent="";
-        muñeco.src="img/desencriptado.jpg";
-    }else{
-        muñeco.src="img/Muñeco.png";
-        txt.textContent="Ningún mensaje fue encontrado";
-        txt2.textContent="Ingrese el texto que desees encriptar o desencriptar.";
-        swal({
-            title:"Debes ingresar algún texto", 
-            icon:"warning",
-            button:"OK"
-        });
-        document.getElementById("mensaje").value="";
+  }
+  
+  function desencriptar() {
+    let texto = document.getElementById("mensaje").value;
+    //remplazar palabras por letras
+    let txtCifrado = texto
+      .replace(/enter/gim, "e")
+      .replace(/ober/gim, "o")
+      .replace(/imes/gim, "i")
+      .replace(/ai/gim, "a")
+      .replace(/ufat/gim, "u");
+  
+    if (texto.length != 0) {
+      document.getElementById("mensaje").value = txtCifrado;
+      document.getElementById("txt").style.display="none";
+      document.getElementById("txt1").style.display="none";
+      document.getElementById("txt2").style.display="none";
+      document.getElementById("txt3").style.display="block";
+      muñeco.src = "img/desencriptado.jpg";
+      return;
+    } else {
+      muñeco.src = "img/Muñeco.png";
+      document.getElementById("mensaje").value = "";
+      document.getElementById("txt").style.display="block";
+      document.getElementById("txt1").style.display="none";
+      document.getElementById("txt2").style.display="block";
+      document.getElementById("txt3").style.display="none";
     }
-
-}
-
-function copiar(){
-    var contenido=document.getElementById("mensaje");
+  }
+  
+  function copiar() {
+    var contenido = document.getElementById("mensaje");
     contenido.select();
     contenido.setSelectionRange(0, 999999);
     document.execCommand("copy");
-}
+  }
